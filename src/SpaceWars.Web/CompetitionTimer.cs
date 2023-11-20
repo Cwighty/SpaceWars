ab2bac4ff18e7c724bda323fca4ea47857ffa24f
Caleb Wightman <agentcboy@gmail.com>
2023-11-19 17:20:29 -0700

    Initial timer logic

---
+++ src/SpaceWars.Web/CompetitionTimer.cs
@@ -0,0 +1,34 @@
+﻿namespace SpaceWars.Web;
+public class CompetitionTimer : SpaceWars.Logic.ITimer
+{
+    private Action tickAction;
+    private Timer timer;
+
+    public CompetitionTimer(GameConfig gameConfig)
+    {
+        Frequency = TimeSpan.FromMilliseconds(gameConfig.TickFrequencyMilliseconds);
+    }
+
+    public TimeSpan Frequency { get; set; }
+
+    public void RegisterAction(Action action)
+    {
+        tickAction = action;
+    }
+
+    public void Start()
+    {
+        if (tickAction == null)
+        {
+            throw new InvalidOperationException("Timer cannot start without a registered tick action.");
+        }
+
+        timer = new Timer(state => tickAction.Invoke(), null, 0, (int)Frequency.TotalMilliseconds);
+    }
+
+    public void Stop()
+    {
+        timer?.Dispose();
+        timer = null;
+    }
+}
